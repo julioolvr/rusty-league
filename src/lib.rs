138f031d2507508008edee1826a88223707744a9
Julio Olivera <julio.olvr@gmail.com>
2017-12-02 15:06:20 -0600

    Added get_players_skills endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,11 @@ pub use responses::*;
 
 const BASE_URL: &'static str = "https://api.rocketleague.com";
 
+#[derive(Serialize)]
+pub struct PlayersQueryBody {
+    player_ids: Vec<PlayerId>,
+}
+
 pub struct LeagueClient {
     token: String,
     core: RefCell<Core>,
@@ -52,7 +57,16 @@ impl LeagueClient {
                              player_id: PlayerId)
                              -> Result<Vec<PlayerSkillResponse>, Error> {
         let path = format!("/api/v1/{}/playerskills/{}", platform.code(), player_id);
-        self.make_request(path)
+        self.make_get_request(path)
+    }
+
+    pub fn get_players_skills(&self,
+                              platform: Platform,
+                              player_ids: Vec<PlayerId>)
+                              -> Result<Vec<PlayerSkillResponse>, Error> {
+        let path = format!("/api/v1/{}/playerskills", platform.code());
+        let query = PlayersQueryBody { player_ids };
+        self.make_post_request(path, serde_json::to_string(&query).unwrap())
     }
 
     pub fn get_player_titles(&self,
@@ -60,17 +74,17 @@ impl LeagueClient {
                              player_id: PlayerId)
                              -> Result<PlayerTitlesResponse, Error> {
         let path = format!("/api/v1/{}/playertitles/{}", platform.code(), player_id);
-        self.make_request(path)
+        self.make_get_request(path)
     }
 
     pub fn get_population(&self) -> Result<PopulationResponse, Error> {
         let path = "/api/v1/population".to_string();
-        self.make_request(path)
+        self.make_get_request(path)
     }
 
     pub fn get_regions(&self) -> Result<Vec<RegionResponse>, Error> {
         let path = "/api/v1/regions".to_string();
-        self.make_request(path)
+        self.make_get_request(path)
     }
 
     pub fn get_skill_leaderboard(&self,
@@ -80,14 +94,14 @@ impl LeagueClient {
         let path = format!("/api/v1/{}/leaderboard/skills/{}",
                            platform.code(),
                            playlist);
-        self.make_request(path)
+        self.make_get_request(path)
     }
 
     pub fn get_stats_leaderboard(&self,
                                  platform: Platform)
                                  -> Result<Vec<StatLeaderboardResponse>, Error> {
         let path = format!("/api/v1/{}/leaderboard/stats", platform.code());
-        self.make_request(path)
+        self.make_get_request(path)
     }
 
     pub fn get_stat_leaderboard(&self,
@@ -95,7 +109,7 @@ impl LeagueClient {
                                 stat: &str)
                                 -> Result<Vec<StatLeaderboardResponse>, Error> {
         let path = format!("/api/v1/{}/leaderboard/stats/{}", platform.code(), stat);
-        self.make_request(path)
+        self.make_get_request(path)
     }
 
     pub fn get_stat_value_for_user(&self,
@@ -107,20 +121,40 @@ impl LeagueClient {
                            platform.code(),
                            stat,
                            player_id);
-        self.make_request(path)
+        self.make_get_request(path)
     }
 
-    fn make_request<T>(&self, path: String) -> Result<T, Error>
+    fn make_get_request<T>(&self, path: String) -> Result<T, Error>
+        where T: serde::de::DeserializeOwned
+    {
+        self.make_request(path, hyper::Method::Get, None)
+    }
+
+    fn make_post_request<T>(&self, path: String, body: String) -> Result<T, Error>
+        where T: serde::de::DeserializeOwned
+    {
+        self.make_request(path, hyper::Method::Post, Some(body))
+    }
+
+    fn make_request<T>(&self,
+                       path: String,
+                       method: hyper::Method,
+                       body: Option<String>)
+                       -> Result<T, Error>
         where T: serde::de::DeserializeOwned
     {
         let uri = format!("{}{}", BASE_URL, path)
             .parse()
             .map_err(|_| Error::Internal)?;
 
-        let mut request: hyper::Request<hyper::Body> = hyper::Request::new(hyper::Method::Get, uri);
+        let mut request: hyper::Request<hyper::Body> = hyper::Request::new(method, uri);
         request
             .headers_mut()
             .set(hyper::header::Authorization(format!("Token {}", self.token)));
+
+        if let Some(body) = body {
+            request.set_body(body)
+        }
 
         let work =
             self.client
