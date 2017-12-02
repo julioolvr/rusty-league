58d51a61789e3290a97b84f6211a867a92e4dd54
Julio Olivera <julio.olvr@gmail.com>
2017-12-02 10:42:55 -0600

    Extract responses module

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ extern crate serde_derive;
 
 mod errors;
 mod platform;
+mod responses;
 
 use std::cell::RefCell;
 use futures::{Future, Stream};
@@ -19,57 +20,9 @@ use tokio_core::reactor::Core;
 
 use errors::Error;
 pub use platform::Platform;
+pub use responses::*;
 
 const BASE_URL: &'static str = "https://api.rocketleague.com";
-
-pub type PlayerId = String;
-pub type PlayerTitle = String;
-
-#[derive(Deserialize, Debug)]
-pub struct PlayerSkillResponse {
-    user_id: Option<String>,
-    user_name: String,
-    player_skills: Vec<PlaylistSkillResponse>,
-    // TODO: Add season_rewards field
-}
-
-#[derive(Deserialize, Debug)]
-struct PlaylistSkillResponse {
-    playlist: i64,
-    tier: Option<i64>,
-    tier_max: Option<i64>,
-    division: Option<i64>,
-    skill: Option<i64>,
-    mu: f64,
-    sigma: f64,
-    win_streak: Option<i64>,
-    matches_played: Option<i64>,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct PlayerTitlesResponse {
-    titles: Vec<PlayerTitle>,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct PopulationResponse {
-    #[serde(rename = "XboxOne")]
-    xbox_one: Vec<PlatformPopulationResponse>,
-    #[serde(rename = "Switch")]
-    switch: Vec<PlatformPopulationResponse>,
-    #[serde(rename = "Steam")]
-    steam: Vec<PlatformPopulationResponse>,
-    #[serde(rename = "PS4")]
-    ps4: Vec<PlatformPopulationResponse>,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct PlatformPopulationResponse {
-    #[serde(rename = "PlaylistID")]
-    playlist: i64,
-    #[serde(rename = "NumPlayers")]
-    num_players: i64,
-}
 
 pub struct LeagueClient {
     token: String,
---
+++ src/responses.rs
@@ -0,0 +1,49 @@
+// TODO: Consider moving this somewhere else
+pub type PlayerId = String;
+pub type PlayerTitle = String;
+
+#[derive(Deserialize, Debug)]
+pub struct PlayerSkillResponse {
+    user_id: Option<String>,
+    user_name: String,
+    player_skills: Vec<PlaylistSkillResponse>,
+    // TODO: Add season_rewards field
+}
+
+#[derive(Deserialize, Debug)]
+struct PlaylistSkillResponse {
+    playlist: i64,
+    tier: Option<i64>,
+    tier_max: Option<i64>,
+    division: Option<i64>,
+    skill: Option<i64>,
+    mu: f64,
+    sigma: f64,
+    win_streak: Option<i64>,
+    matches_played: Option<i64>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PlayerTitlesResponse {
+    titles: Vec<PlayerTitle>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PopulationResponse {
+    #[serde(rename = "XboxOne")]
+    xbox_one: Vec<PlatformPopulationResponse>,
+    #[serde(rename = "Switch")]
+    switch: Vec<PlatformPopulationResponse>,
+    #[serde(rename = "Steam")]
+    steam: Vec<PlatformPopulationResponse>,
+    #[serde(rename = "PS4")]
+    ps4: Vec<PlatformPopulationResponse>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PlatformPopulationResponse {
+    #[serde(rename = "PlaylistID")]
+    playlist: i64,
+    #[serde(rename = "NumPlayers")]
+    num_players: i64,
+}
