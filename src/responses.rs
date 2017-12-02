76a684b758c1a40054e59d36b2e727847fccccff
Julio Olivera <julio.olvr@gmail.com>
2017-12-02 15:36:57 -0600

    Made response structs fields public

--- src/responses.rs
+++ src/responses.rs
@@ -4,94 +4,94 @@ pub type PlayerTitle = String;
 
 #[derive(Deserialize, Debug)]
 pub struct PlayerSkillResponse {
-    user_id: Option<String>,
-    user_name: String,
-    player_skills: Vec<PlaylistSkillResponse>,
+    pub user_id: Option<String>,
+    pub user_name: String,
+    pub player_skills: Vec<PlaylistSkillResponse>,
     // TODO: Add season_rewards field
 }
 
 #[derive(Deserialize, Debug)]
-struct PlaylistSkillResponse {
-    playlist: i64,
-    tier: Option<i64>,
-    tier_max: Option<i64>,
-    division: Option<i64>,
-    skill: Option<i64>,
-    mu: f64,
-    sigma: f64,
-    win_streak: Option<i64>,
-    matches_played: Option<i64>,
+pub struct PlaylistSkillResponse {
+    pub playlist: i64,
+    pub tier: Option<i64>,
+    pub tier_max: Option<i64>,
+    pub division: Option<i64>,
+    pub skill: Option<i64>,
+    pub mu: f64,
+    pub sigma: f64,
+    pub win_streak: Option<i64>,
+    pub matches_played: Option<i64>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct PlayerTitlesResponse {
-    titles: Vec<PlayerTitle>,
+    pub titles: Vec<PlayerTitle>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct PopulationResponse {
     #[serde(rename = "XboxOne")]
-    xbox_one: Vec<PlatformPopulationResponse>,
+    pub xbox_one: Vec<PlatformPopulationResponse>,
     #[serde(rename = "Switch")]
-    switch: Vec<PlatformPopulationResponse>,
+    pub switch: Vec<PlatformPopulationResponse>,
     #[serde(rename = "Steam")]
-    steam: Vec<PlatformPopulationResponse>,
+    pub steam: Vec<PlatformPopulationResponse>,
     #[serde(rename = "PS4")]
-    ps4: Vec<PlatformPopulationResponse>,
+    pub ps4: Vec<PlatformPopulationResponse>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct PlatformPopulationResponse {
     #[serde(rename = "PlaylistID")]
-    playlist: i64,
+    pub playlist: i64,
     #[serde(rename = "NumPlayers")]
-    num_players: i64,
+    pub num_players: i64,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct RegionResponse {
-    platforms: String,
-    region: String, // TODO: Consider an enum
+    pub platforms: String,
+    pub region: String, // TODO: Consider an enum
 }
 
 #[derive(Deserialize, Debug)]
 pub struct SkillLeaderboardResponse {
-    user_id: Option<String>,
-    user_name: String,
-    tier: i64,
-    skill: i64,
+    pub user_id: Option<String>,
+    pub user_name: String,
+    pub tier: i64,
+    pub skill: i64,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct StatLeaderboardResponse {
-    stats: Vec<PlayerStatResponse>,
-    stat_type: String,
+    pub stats: Vec<PlayerStatResponse>,
+    pub stat_type: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct PlayerStatResponse {
-    user_id: Option<String>,
-    user_name: String,
-    assists: Option<i64>,
-    goals: Option<i64>,
-    mvps: Option<i64>,
-    saves: Option<i64>,
-    shots: Option<i64>,
-    wins: Option<i64>,
+    pub user_id: Option<String>,
+    pub user_name: String,
+    pub assists: Option<i64>,
+    pub goals: Option<i64>,
+    pub mvps: Option<i64>,
+    pub saves: Option<i64>,
+    pub shots: Option<i64>,
+    pub wins: Option<i64>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct StatValueForUserResponse {
-    user_id: Option<String>,
-    user_name: Option<String>,
-    stat_type: String,
-    value: String,
+    pub user_id: Option<String>,
+    pub user_name: Option<String>,
+    pub stat_type: String,
+    pub value: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct StatValueForUserMultipleResponse {
-    user_id: Option<String>,
-    user_name: Option<String>,
-    stat_type: String,
-    value: i64,
+    pub user_id: Option<String>,
+    pub user_name: Option<String>,
+    pub stat_type: String,
+    pub value: i64,
 }
