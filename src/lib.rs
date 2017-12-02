b1cf2e43702d8e3c79d2b021e44e43faa1259812
Julio Olivera <julio.olvr@gmail.com>
2017-12-02 01:07:18 -0600

    First working implementation for one request

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,120 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio_core;
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
+use std::cell::RefCell;
+use futures::{Future, Stream};
+use hyper::{Client, Chunk};
+use hyper_tls::HttpsConnector;
+use tokio_core::reactor::Core;
+
+const BASE_URL: &'static str = "https://api.rocketleague.com";
+
+#[derive(Debug)]
+pub enum Error {
+    Internal,
+}
+
+pub enum Platform {
+    Steam,
+    Xbox,
+    Playstation,
+}
+
+impl Platform {
+    pub fn code(&self) -> String {
+        match self {
+                &Platform::Steam => "steam",
+                &Platform::Xbox => "xboxone",
+                &Platform::Playstation => "ps4",
+            }
+            .to_string()
+    }
+}
+
+pub type PlayerId = String;
+
+#[derive(Deserialize, Debug)]
+pub struct PlayerSkillResponse {
+    user_id: Option<String>,
+    user_name: String,
+    player_skills: Vec<PlaylistSkillResponse>,
+    // TODO: Add season_rewards field
+}
+
+#[derive(Deserialize, Debug)]
+struct PlaylistSkillResponse {
+    playlist: i64,
+    tier: Option<i64>,
+    tier_max: Option<i64>,
+    division: Option<i64>,
+    skill: Option<i64>,
+    mu: f64,
+    sigma: f64,
+    win_streak: Option<i64>,
+    matches_played: Option<i64>,
+}
+
+pub struct LeagueClient {
+    token: String,
+    core: RefCell<Core>,
+    client: Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>>,
+}
+
+impl LeagueClient {
+    pub fn new(token: String) -> Result<LeagueClient, Error> {
+        let core = Core::new().map_err(|_| Error::Internal)?;
+        let handle = core.handle();
+
+        let client = Client::configure()
+            .connector(HttpsConnector::new(4, &handle)
+                           .map_err(|_| Error::Internal)?)
+            .build(&handle);
+
+        Ok(LeagueClient {
+               token,
+               core: RefCell::new(core),
+               client,
+           })
+    }
+
+    pub fn get_player_skills(&self,
+                             platform: Platform,
+                             player_id: PlayerId)
+                             -> Result<Vec<PlayerSkillResponse>, Error> {
+        let path = format!("/api/v1/{}/playerskills/{}", platform.code(), player_id);
+        self.make_request(path)
+    }
+
+    fn make_request<T>(&self, path: String) -> Result<T, Error>
+        where T: serde::de::DeserializeOwned
+    {
+        let uri = format!("{}{}", BASE_URL, path)
+            .parse()
+            .map_err(|_| Error::Internal)?;
+
+        let mut request: hyper::Request<hyper::Body> = hyper::Request::new(hyper::Method::Get, uri);
+        request
+            .headers_mut()
+            .set(hyper::header::Authorization(format!("Token {}", self.token)));
+
+        let work = self.client
+            .request(request)
+            .and_then(|res| res.body().concat2())
+            .and_then(move |body: Chunk| {
+                          let v = serde_json::from_slice(&body).unwrap();
+                          Ok(v)
+                      });
+
+        self.core
+            .borrow_mut()
+            .run(work)
+            .map_err(|_| Error::Internal)
     }
 }
